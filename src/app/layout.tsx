bda3e1da91473454c7e62f0c9dc899f264f56e47
Vitalii <115796654+Vitaliqu@users.noreply.github.com>
2025-10-27 14:27:51 -0400

    predeploy_build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,21 @@ import type {Metadata} from "next";
 export const metadata: Metadata = {
     title: "Стоматологія Колібрі",
     description: "Стоматологія Колібрі у Хусті — сучасна клініка з турботою про вашу усмішку. Професійне лікування зубів, імплантація, протезування, гігієна та дитяча стоматологія!",
+    openGraph: {
+        title: "Стоматологія Колібрі | Хуст",
+        description: "Безболісна сучасна стоматологія у Хусті.",
+        siteName: "Стоматологія Колібрі",
+        images: [
+            {
+                url: '../components/clinic.jpg',
+                width: 1200,
+                height: 630,
+                alt: "Стоматологія Колібрі — сучасна клініка у Хусті",
+            },
+        ],
+        locale: "uk_UA",
+        type: "website",
+    },
 };
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
