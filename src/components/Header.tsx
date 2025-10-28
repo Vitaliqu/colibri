b0886d55aa6b78ceb3f75235c68188e44b0aa774
Vitalii <115796654+Vitaliqu@users.noreply.github.com>
2025-10-28 15:27:34 -0400

    minor_fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,6 +43,7 @@ export const Header: React.FC = () => {
         <header
             className="fixed top-0 left-0 right-0 py-2 bg-white/90 backdrop-blur-md border-b border-gray-100 z-50 transition-all duration-300">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-14">
+                <link rel="icon" href="../../public/favicon.ico"/>
                 {/* Logo */}
                 <Link href="/" className="flex items-center space-x-0">
                     <Image src={logo} alt="Логотип" className="w-16 mb-1"/>
